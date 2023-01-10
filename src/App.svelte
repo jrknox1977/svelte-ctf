c2dd204bb98c15cffd36d90bd4ad9c4d9cdbec55
Josh <jrknox1977@gmail.com>
2023-01-10 16:30:01 -0500

    svelte-ctf

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Styles } from 'sveltestrap';
+  import NavBar from './lib/NavBar.svelte';
+
+  let count = 0;
+
+  function increment() {
+    count += 1;
+  }
+</script>
+<head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"/>
+</head>
+
+<main>
+  <h1>Svelte CTF</h1>
+  <NavBar />
+  <p>Counter = {count}</p>
+  <button on:click={ increment }>Increment</button>
+</main>
+
+<style>
+ 
+</style>
---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+  } from 'sveltestrap';
+
+  let isOpen = false;
+
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
+</script>
+<head>
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
+</head>
+
+<Navbar color="light" light expand="md">
+  <NavbarBrand href="/">Svelte-CTF</NavbarBrand>
+  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+    <Nav class="ms-auto" navbar>
+      <NavItem>
+        <NavLink href="#components/">Components</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
+      </NavItem>
+      <Dropdown nav inNavbar>
+        <DropdownToggle nav caret>Options</DropdownToggle>
+        <DropdownMenu end>
+          <DropdownItem>Option 1</DropdownItem>
+          <DropdownItem>Option 2</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Reset</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </Nav>
+  </Collapse>
+</Navbar>
